95b3f98761fba6e9e9e5d6ed8d8c4751157b30d6
Eleonora Dyrda <elyadirda@yandex.ru>
2022-03-15 16:27:58 +0300

    models

--- backend/foodgram/recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('recipe/', views.recipe_list),
-    path('recipe/<pk>/', views.recipe_detail),
-]
