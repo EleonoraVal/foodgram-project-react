c59e82b0dc9035e18da43d4fadab6c74676766c0
Eleonora Dyrda <elyadirda@yandex.ru>
2022-04-22 17:00:37 +0300

    allowed hosts

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '78.47.203.217', 'myfoodgramnetwork.ddns.net']
 
 
 INSTALLED_APPS = [
